31da39a7b22b97b0f9957f0b369eb57a12b038e4
Reinan <reinanlopes.1@gmail.com>
2025-02-04 22:03:54 -0300

    Refactor LED control logic to use RGB color settings and update pin assignments

--- U4C5-1.c
+++ U4C5-1.c
@@ -2,44 +2,45 @@
 #include <stdlib.h>
 #include "pico/stdlib.h"
 
-#define RED_LED_PIN 11
-#define YELLOW_LED_PIN 12
+#define BLUE_LED_PIN 11
+#define RED_LED_PIN 12
 #define GREEN_LED_PIN 13
 
 void setup() {
     stdio_init_all();
     gpio_init(RED_LED_PIN);
     gpio_set_dir(RED_LED_PIN, GPIO_OUT);
-    gpio_init(YELLOW_LED_PIN);
-    gpio_set_dir(YELLOW_LED_PIN, GPIO_OUT);
     gpio_init(GREEN_LED_PIN);
     gpio_set_dir(GREEN_LED_PIN, GPIO_OUT);
+    gpio_init(BLUE_LED_PIN);
+    gpio_set_dir(BLUE_LED_PIN, GPIO_OUT);
 }
 
 void turn_off_all_leds() {
     gpio_put(RED_LED_PIN, 0);
-    gpio_put(YELLOW_LED_PIN, 0);
     gpio_put(GREEN_LED_PIN, 0);
+    gpio_put(BLUE_LED_PIN, 0);
 }
 
-void turn_on_led(uint led_pin) {
-    turn_off_all_leds();
-    gpio_put(led_pin, 1);
+void set_led_color(bool red, bool green, bool blue) {
+    gpio_put(RED_LED_PIN, red);
+    gpio_put(GREEN_LED_PIN, green);
+    gpio_put(BLUE_LED_PIN, blue);
 }
 
 bool repeating_timer_callback(struct repeating_timer *t) {
     static int state = 0;
     switch (state) {
         case 0:
-            turn_on_led(RED_LED_PIN);
+            set_led_color(false, true, false); // Vermelho
             state = 1;
             break;
         case 1:
-            turn_on_led(YELLOW_LED_PIN);
+            set_led_color(false, true, true); // Amarelo (vermelho + verde)
             state = 2;
             break;
         case 2:
-            turn_on_led(GREEN_LED_PIN);
+            set_led_color(false, false, true); // Verde
             state = 0;
             break;
     }
@@ -48,7 +49,7 @@ bool repeating_timer_callback(struct repeating_timer *t) {
 
 int main() {
     setup();
-    turn_on_led(RED_LED_PIN);
+    set_led_color(true, false, false); // Inicia com vermelho
 
     struct repeating_timer timer;
     add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
